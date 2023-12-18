c4d84e1f8f3afa5674b88bf63d26ebc292bfc3ba
Saksham1411 <sakshamsamgarg1411@gmail.com>
2023-12-18 14:45:05 +0530

    path

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ const staticRoute = require('./routes/staticRoutes');
 const studentRoute = require('./routes/student');
 const notFound = require('./middleware/not-found');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.set("view engine", "hbs");
-app.set("views", "./views");
+app.set("views", path.resolve("./views"));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));

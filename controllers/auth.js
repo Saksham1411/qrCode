6f53434c403a8a213be53641878d9bfdff3dfbe6
Saksham1411 <sakshamsamgarg1411@gmail.com>
2023-12-26 18:49:22 +0530

    bug fix

--- controllers/auth.js
+++ controllers/auth.js
@@ -9,7 +9,7 @@ const register = async (req, res) => {
     }
     const user = await User.create({ ...req.body });
     const token = user.createJWT();
-    res.status(StatusCodes.CREATED).send({ user, token });
+    res.status(StatusCodes.CREATED).redirect('/signin');
 }
 
 const logIn = async (req, res) => {
